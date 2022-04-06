033360f4ea916abba4b471a8322e2f7c65812303
Anders Søgaard <andershagbard@gmail.com>
2022-04-06 16:17:58 +0200

    Try to update binary

--- src/actions/parts/upload-zip.ts
+++ src/actions/parts/upload-zip.ts
@@ -3,6 +3,7 @@ import fs from 'fs-extra';
 import ngrok from 'ngrok';
 import { createTheme } from '../../helpers/shopify';
 import { ThemeRole } from '../../types/shopify';
+import { updateNgrokBinary } from '../../helpers/ngrok';
 
 export default async (path: string, data: { name: string, role: ThemeRole }): Promise<number> => {
   // Start tunnel
@@ -19,6 +20,8 @@ export default async (path: string, data: { name: string, role: ThemeRole }): Pr
   }).listen(8080); */
 
   console.log({ path, data });
+
+  await updateNgrokBinary();
 
   try {
     const url = (await ngrok.connect(8080)).replace('https://', 'http://');
