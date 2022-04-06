708b8b5e8061e7bc8bd9a1a49a6314d997802bec
andershagbard <andershagbard@gmail.com>
2022-04-06 13:05:23 +0200

    Define inputs

--- src/inputs.ts
+++ src/inputs.ts
@@ -5,21 +5,21 @@ export const ACTION = core.getInput('ACTION', {
   trimWhitespace: true,
 }) as 'PREVIEW' | 'DEPLOY' | 'DELETE';
 
-export const SHOPIFY_STORE: string = core.getInput('SHOPIFY_STORE', {
-  required: true,
-  trimWhitespace: true,
-});
-
-export const SHOPIFY_ACCESS_TOKEN: string = core.getInput('SHOPIFY_ACCESS_TOKEN', {
-  required: true,
-  trimWhitespace: true,
-});
-
-export const GITHUB_TOKEN: string = core.getInput('GITHUB_TOKEN', {
-  required: false,
-});
-
-export const SHOPIFY_ENVIRONMENT: string = core.getInput('SHOPIFY_ENVIRONMENT', {
+export const THEME_KIT_ENVIRONMENT: string = core.getInput('THEME_KIT_ENVIRONMENT', {
   required: false,
   trimWhitespace: true,
 }) ?? 'development';
+
+export const SHOPIFY_THEME_ROLE: string = core.getInput('SHOPIFY_THEME_ROLE', {
+  required: false,
+  trimWhitespace: true,
+}) ?? 'development';
+
+export const GITHUB_TOKEN: string = core.getInput('GITHUB_TOKEN', {
+  required: true,
+});
+
+export const BUILD_DIR: string = core.getInput('GITHUB_TOKEN', {
+  required: false,
+  trimWhitespace: true,
+}) ?? 'build';
