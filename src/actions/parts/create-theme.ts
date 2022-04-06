8f4dabca52dfc4d45e4bddd4d0c7d9cce1579c5e
Anders Søgaard <andershagbard@gmail.com>
2022-04-06 18:43:33 +0200

    Add info

--- src/actions/parts/create-theme.ts
+++ src/actions/parts/create-theme.ts
@@ -1,3 +1,4 @@
+import * as core from '@actions/core';
 import themeKit from '@shopify/themekit';
 import { createTheme } from '../../helpers/shopify';
 import { ThemeRole } from '../../types/shopify';
@@ -7,10 +8,12 @@ import config from '../../helpers/config';
 const environment = config[THEME_KIT_ENVIRONMENT];
 
 export default async (data: { name: string, role: ThemeRole }): Promise<number> => {
+  core.info('Creating theme...');
   const response = await createTheme({
     src: 'https://github.com/Shopify/dawn/archive/refs/heads/main.zip',
     ...data,
   });
+  core.info(`Theme created with ID: ${response.theme.id}`);
 
   themeKit.command('deploy', {
     dir: BUILD_DIR,
