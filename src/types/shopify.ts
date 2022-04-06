c0230eecb7ea66c856d5bfb77832bfac725479cd
andershagbard <andershagbard@gmail.com>
2022-04-06 13:07:48 +0200

    Add Shopify types

---
+++ src/types/shopify.ts
@@ -0,0 +1,21 @@
+export type ThemeRole = 'main' | 'unpublished' | 'demo' | 'development';
+
+export type Theme = {
+  id: number
+  name: string
+  created_at: string
+  updated_at: string
+  role: ThemeRole
+  previewable: false
+  processing: true
+};
+
+export type CreateTheme = {
+  name: string
+  src: string
+  role: ThemeRole
+};
+
+export type Asset = {
+  key: string
+};
