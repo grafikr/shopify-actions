8e7b7dfa728414880dc71e423794acb5140a3c56
andershagbard <andershagbard@gmail.com>
2022-04-06 13:07:57 +0200

    Add Github helper

---
+++ src/helpers/github.ts
@@ -0,0 +1,29 @@
+import * as github from '@actions/github';
+import { GITHUB_TOKEN } from '../inputs';
+
+const octokit = github.getOctokit(GITHUB_TOKEN);
+const { context } = github;
+
+export const getExistingComment = async () => {
+  const comments = await octokit.rest.issues.listComments({
+    ...github.context.repo,
+    issue_number: context.payload.pull_request.number,
+  });
+
+  return comments.data.find((comment) => comment.body_text.startsWith('Theme created'));
+};
+
+export const createComment = async (body: string) => octokit.rest.issues.createComment({
+  ...context.repo,
+  issue_number: context.payload.pull_request.number,
+  body,
+});
+
+export const updateComment = async (commentID: number, body: string) => {
+  octokit.rest.issues.updateComment({
+    ...context.repo,
+    issue_number: context.payload.pull_request.number,
+    comment_id: commentID,
+    body,
+  });
+};
