4d070bfa7542d90ac356e76b28ab991017e9acf6
andershagbard <andershagbard@gmail.com>
2022-04-07 15:58:22 +0200

    Revert test changes

--- src/actions/parts/upload-zip.ts
+++ src/actions/parts/upload-zip.ts
@@ -6,7 +6,7 @@ import { ThemeRole } from '../../types/shopify';
 
 export default async (path: string, data: { name: string, role: ThemeRole }): Promise<number> => {
   // Start tunnel
-  /* const server = http.createServer((request, response) => {
+  const server = http.createServer((request, response) => {
     const stat = fs.statSync(path);
 
     response.writeHead(200, {
@@ -16,22 +16,8 @@ export default async (path: string, data: { name: string, role: ThemeRole }): Pr
 
     const stream = fs.createReadStream(path);
     stream.pipe(response);
-  }).listen(8080); */
+  }).listen(8080);
 
-  console.log({ path, data });
-
-  try {
-    const url = (await ngrok.connect(8080)).replace('https://', 'http://');
-    console.log(url);
-  } catch (error) {
-    console.log({ error });
-
-    throw error;
-  }
-
-  return 123;
-
-/*
   // Send create theme request
   const response = await createTheme({
     src: (await ngrok.connect(8080)).replace('https://', 'http://'),
@@ -45,5 +31,4 @@ export default async (path: string, data: { name: string, role: ThemeRole }): Pr
   await ngrok.kill();
 
   return response.theme.id;
- */
 };
