398ee3e6c12ea89ba95bc4c1c9bab23d8d671e0e
andershagbard <andershagbard@gmail.com>
2022-04-21 09:42:25 +0200

    Remove unused

--- src/actions/preview.ts
+++ src/actions/preview.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core';
-import * as github from '@actions/github';
 import { BUILD_DIR, SHOPIFY_THEME_ROLE } from '../inputs';
 import { getCustomizeURL, getPreviewURL, getTheme } from '../helpers/shopify';
 import buildFromEnvironment from './parts/build-from-environment';
