98b0297a98a7814102602e37ffb4413c914a9905
Anders Søgaard <9662430+andershagbard@users.noreply.github.com>
2023-07-12 07:43:33 +0200

    Wrap in promise and refactor end

--- src/actions/parts/create-zip-from-build.ts
+++ src/actions/parts/create-zip-from-build.ts
@@ -5,14 +5,18 @@ import path from 'path';
 import { BUILD_DIR } from '../../inputs';
 
 export default async (): Promise<string> => {
-  core.info(`Creating zip file from directory "${BUILD_DIR}"`);
+  return new Promise(async (resolve) => {
+    core.info(`Creating zip file from directory "${BUILD_DIR}"`);
 
-  const zip = fs.createWriteStream('build.zip');
-  const archive = archiver('zip', { zlib: { level: 9 } });
-  archive.pipe(zip);
+    const zip = fs.createWriteStream('build.zip');
+    const archive = archiver('zip', { zlib: { level: 9 } });
+    archive.pipe(zip);
 
-  archive.directory(BUILD_DIR, false);
-  await archive.finalize();
+    zip.on('close', () => {
+      resolve(path.resolve('build.zip'));
+    });
 
-  return path.resolve('build.zip');
+    archive.directory(BUILD_DIR, false);
+    await archive.finalize();
+  });
 };
