c93fc180dbb905d5c0f708e499fbe89a501bec50
Anders Søgaard <andershagbard@gmail.com>
2024-07-17 09:00:09 +0200

    Update Shopify API version

--- src/helpers/themekit.ts
+++ src/helpers/themekit.ts
@@ -11,10 +11,10 @@ export const isThemeKitToken = (token: string): boolean =>
   token.startsWith('shptka_');
 
 export const themeKitBaseURL = () =>
-  'https://theme-kit-access.shopifyapps.com/cli/admin/api/2022-04/';
+  'https://theme-kit-access.shopifyapps.com/cli/admin/api/2024-07/';
 
 export const shopifyBaseURL = (store: string) =>
-  `https://${store}/admin/api/2022-04/`;
+  `https://${store}/admin/api/2024-07/`;
 
 export const deployTheme = async (themeID: number) => {
   try {
